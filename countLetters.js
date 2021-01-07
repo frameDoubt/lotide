8f5c93e52bb174ceb476948e65639e158d30aa13
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-07 14:44:15 +0000

    added function that counts the characters of a string and returns an object with the count of each character

---
+++ countLetters.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function (string) {
+  let modifiedInput = string.replace(/ /g, "");
+  let results = {};
+  for (let letter of modifiedInput) {
+    if (results[letter]) {
+      results[letter] += 1;
+    } else {
+      results[letter] = 1;
+    }
+  }
+  return results;
+};
+
+console.log(countLetters("this is so stupid"));
