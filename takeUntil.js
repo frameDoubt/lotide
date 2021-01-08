9eb2cc73a000ecec781e5564483ba8b9ba91e0bf
Brandon Wright <brandon.w.wright4@gmail.com>
2021-01-08 11:18:29 +0000

    added a function that is fundamentally similar to lotides takewhile function

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+// functions used for test actual function
+const eqArrays = function (array1, array2) {
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function (firstArray, secondArray) {
+  console.log(eqArrays(firstArray, secondArray));
+};
+
+// actual exercise function
+const takeUntil = function(array, callback) {
+  const result = [];
+  for (let value of array) {
+    if (callback(value)) {
+      return result
+    } else {
+      result.push(value);
+    }
+  }
+};
+
+// testing code
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
